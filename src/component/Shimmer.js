61d93c67fd989a50e079e72e9a9b7c2fa904e685
Dipesh Shah <deepace780@gmail.com>
2023-02-02 22:59:45 +0545

    fix: fixed error related to keys

--- src/component/Shimmer.js
+++ src/component/Shimmer.js
@@ -3,7 +3,7 @@ const Shimmer = () => {
     <div className="flex flex-wrap justify-evenly pl-32 pr-32 pt-5">
       {Array(16)
         .fill("")
-        .map((index) => (
+        .map((e, index) => (
           <div className="w-[300px] h-[300px]" key={index}>
             <p className=" bg-gray-100 rounded-md h-[190px]"></p>
             <p className="bg-gray-100 w-40 h-4 mt-5 "></p>
