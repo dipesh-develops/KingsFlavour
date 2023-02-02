8dfff80cdbf9a709fc62c1afd7f7b878fd99777f
Dipesh Shah <deepace780@gmail.com>
2023-02-02 22:59:19 +0545

    added code splitting/lazy loading

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Body from "./component/Body";
 import Footer from "./component/Footer";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import ErrorPage from "./component/ErrorPage";
-import About from "./component/About";
 import Contact from "./component/Contact";
 import RestaurantMenu from "./component/RestaurantMenu";
 import Profile from "./component/Profile";
+import { lazy, Suspense } from "react";
+import About from "./component/About";
+import Shimmer from "./component/Shimmer";
+// Chunking
+// Code Splitting
+// Dynamic Bundling
+// Lazy Loading
+// On Demand Loading
+// Dynamic Import
+// const About = lazy(() => import("./component/About"));
+const Instamart = lazy(() => import("./component/Instamart"));
 
 const Layout = () => {
   return (
@@ -47,6 +57,14 @@ const router = createBrowserRouter([
       {
         path: "/restaurant/:id",
         element: <RestaurantMenu />,
+      },
+      {
+        path: "/instamart",
+        element: (
+          <Suspense fallback={<Shimmer />}>
+            <Instamart />
+          </Suspense>
+        ),
       },
     ],
   },
