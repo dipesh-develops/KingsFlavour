6d640ddd180baf5a124d379e0dd38970117ae137
Dipesh Shah <deepace780@gmail.com>
2023-02-02 12:18:40 +0545

    added shimmer styles

--- src/component/Shimmer.js
+++ src/component/Shimmer.js
@@ -1,13 +1,13 @@
 const Shimmer = () => {
   return (
-    <div className="list">
+    <div className="flex flex-wrap justify-evenly pl-32 pr-32 pt-5">
       {Array(16)
         .fill("")
         .map((index) => (
-          <div className="shimmerCard" key={index}>
-            <p></p>
-            <p></p>
-            <p></p>
+          <div className="w-[300px] h-[300px]" key={index}>
+            <p className=" bg-gray-100 rounded-md h-[190px]"></p>
+            <p className="bg-gray-100 w-40 h-4 mt-5 "></p>
+            <p className="bg-gray-100 w-28 h-4 mt-3 "></p>
           </div>
         ))}
     </div>
