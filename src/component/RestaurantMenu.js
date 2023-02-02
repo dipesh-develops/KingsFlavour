2365e9c1d27dbf27bacc22a058cc4e2aa63028be
Dipesh Shah <deepace780@gmail.com>
2023-02-02 23:00:59 +0545

    feat: creaed custom hook to fetch data from api for restaurant menu

--- src/component/RestaurantMenu.js
+++ src/component/RestaurantMenu.js
@@ -1,27 +1,12 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IMG_CDN_URL } from "../contants";
 import Shimmer from "./Shimmer";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [restaurant, setRestaurant] = useState(null);
   const { id } = useParams();
-  console.log("use");
-  console.log(useState());
-  useEffect(() => {
-    getRestaurantsMenu();
-  }, []);
-
-  async function getRestaurantsMenu() {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/v4/full?lat=28.6105073&lng=77.1145653&menuId=" +
-        id
-    );
-    const json = await data.json();
-    setRestaurant(json.data);
-    console.log("restaurant menu");
-    console.log(json);
-  }
+  // imported custom hook to get restaurant data
+  const restaurant = useRestaurantMenu(id);
   return !restaurant ? (
     <Shimmer />
   ) : (
